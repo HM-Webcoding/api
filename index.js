b5c78978785e4c187961fdd089fdfe93857b3482
Hamidur Rashid <hamidmonju01@gmail.com>
2023-09-25 11:43:28 +0600

    playes

--- index.js
+++ index.js
@@ -7,36 +7,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const people = [
+const circketPlayerBD = [
   {
-    Name: "Sakib",
+    Name: "Tamim Iqbal Khan",
+    Age: 34,
+    Dob: "March 20, 1989",
+    PlayingRole: "Opening Batter",
+    id: 1,
+  },
+  {
+    Name: "Mohammad Ashraful",
+    Age: 39,
+    Dob: "July 07, 1984",
+    PlayingRole: "Middle order Batter",
+    id: 2,
+  },
+  {
+    Name: "Shakib Al Hasan",
+    Age: 36,
+    Dob: "March 24, 1987",
+    PlayingRole: "Allrounder",
+    id: 3,
+  },
+  {
+    Name: "Taskin Ahmed",
+    Age: 20,
+    Dob: "April 03, 1995",
+    PlayingRole: "Bowler",
+    id: 4,
+  },
+  {
+    Name: "Litton Kumer Das",
     Age: 28,
-    Dob: "12 march 1995",
-  },
-  {
-    Name: "Tamim",
-    Age: 33,
-    Dob: "12 march 1990",
-  },
-  {
-    Name: "Marshafi",
-    Age: 30,
-    Dob: "12 march 1995",
-  },
-  {
-    Name: "Mosfiqe",
-    Age: 32,
-    Dob: "12 march 1995",
-  },
-  {
-    Name: "Taskin",
-    Age: 26,
-    Dob: "12 march 1995",
+    Dob: "October 13, 1994",
+    PlayingRole: "Wicketkeeper Batter",
+    id: 5,
   },
 ];
 
 app.get("/", (req, res) => {
-  res.json(people);
+  res.json(circketPlayerBD);
 });
 
 app.listen(8000, () => {
